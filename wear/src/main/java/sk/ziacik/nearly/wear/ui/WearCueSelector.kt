e73ead949a4ecb8b9ac71f5cdd5e0c349d44db59
František Žiačik <ziacik@gmail.com>
2026-09-07 17:46:50 +0200

    fix: remove invalid wear weight import

--- wear/src/main/java/sk/ziacik/nearly/wear/ui/WearCueSelector.kt
+++ wear/src/main/java/sk/ziacik/nearly/wear/ui/WearCueSelector.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
